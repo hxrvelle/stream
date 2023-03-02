141b9c4d9def02bebb12b33cd0eafbab98fd4e59
hxrvelle <122966458+hxrvelle@users.noreply.github.com>
2023-03-02 12:10:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -31,7 +31,6 @@ public class Main {
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-
                 }
             }
         } catch (Exception e) {
